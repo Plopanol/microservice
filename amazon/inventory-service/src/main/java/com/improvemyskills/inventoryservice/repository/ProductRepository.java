3b8f32b687f2a49b53bf08f768ae7d3a649d0a69
olivier.rombaut <olivier.rombaut@protonmail.ch>
2024-10-02 10:31:27 +0200

    On repart de 0

--- amazon/inventory-service/src/main/java/com/improvemyskills/inventoryservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.improvemyskills.inventoryservice.repository;
-
-import com.improvemyskills.inventoryservice.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//@RepositoryRestResource
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
